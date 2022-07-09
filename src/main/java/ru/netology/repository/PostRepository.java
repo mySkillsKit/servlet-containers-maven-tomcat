5c21bfbd45d9df5888a4fed7b7b6557799d592ce
Anatolii Vasilev <anatmainbox@gmail.com>
2022-07-09 22:52:53 +0400

    add Annotation Config

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
@@ -9,6 +10,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 // Stub
+@Repository
 public class PostRepository {
 
     private Map<Long, Post> postsListMap = new ConcurrentHashMap<>();
