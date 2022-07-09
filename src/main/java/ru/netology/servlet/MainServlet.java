163b10525a6991b191789d937192a9b87293aba3
Anatolii Vasilev <anatmainbox@gmail.com>
2022-07-09 23:19:52 +0400

    add JavaConfig.java

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
 import ru.netology.exception.NotFoundException;
 import ru.netology.repository.PostRepository;
@@ -16,9 +18,10 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        // отдаём класс конфигурации
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        // получаем по классу бина
+        controller = context.getBean(PostController.class);
     }
 
     @Override
